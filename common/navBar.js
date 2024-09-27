cf670c29473a7f3c34f0a83369e994bda7247753
Joe Sharp <jsharp@caci.co.uk>
2024-09-27 07:50:49 +0100

    fixing nav

--- common/navBar.js
+++ common/navBar.js
@@ -1,58 +1,58 @@
 const NAV_ITEMS = [
-	{
-		title: 'Home',
-		href: '/index.html'
-	},
-	{
-		title: 'Networks',
-		href: '/networks/index.html'
-	},
-	{
-		title: 'A Level',
-		href: '/a-level/index.html'
-	},
-	{
-		title: 'Industry Profiles',
-		href: '/industry-profiles/'
-	},
-	{
-		title: 'Software Development Lifecycle',
-		href: '/industry-profiles/software-dev-lifecycle.html'
-	},
-	{
-		title: 'Team Structure',
-		href: '/industry-profiles/team-structure.html'
-	}
-
-]
+  {
+    title: "Home",
+    href: "index.html",
+  },
+  {
+    title: "Networks",
+    href: "networks/index.html",
+  },
+  {
+    title: "A Level",
+    href: "a-level/index.html",
+  },
+  {
+    title: "Industry Profiles",
+    href: "industry-profiles/",
+  },
+  {
+    title: "Software Development Lifecycle",
+    href: "industry-profiles/software-dev-lifecycle.html",
+  },
+  {
+    title: "Team Structure",
+    href: "industry-profiles/team-structure.html",
+  },
+];
 
 class NavBar extends HTMLElement {
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-	connectedCallback() {
-		// Create a shadow root
-		const shadow = this.attachShadow({ mode: "open" });
-		const items = this.getAttribute('navItems');
-		const current = this.getAttribute('current');
+  connectedCallback() {
+    // Create a shadow root
+    const shadow = this.attachShadow({ mode: "open" });
+    const items = this.getAttribute("navItems");
+    const current = this.getAttribute("current");
+    const relative = this.getAttribute("relative") || ".";
 
-		// Create spans
-		const wrapper = document.createElement("ul");
-		for (const navItem of NAV_ITEMS) {
-			const li = document.createElement('li');
-			if (current === navItem.title) {
-				li.classList.add('currentNav');
-			}
-			const anchor = document.createElement('a');
-			anchor.innerText = navItem.title;
-			anchor.setAttribute('href', navItem.href);
-			li.appendChild(anchor);
-			wrapper.appendChild(li);
-		}
+    // Create spans
+    const wrapper = document.createElement("ul");
+    for (const navItem of NAV_ITEMS) {
+      const li = document.createElement("li");
+      if (current === navItem.title) {
+        li.classList.add("currentNav");
+      }
+      const anchor = document.createElement("a");
+      anchor.innerText = navItem.title;
+      anchor.setAttribute("href", relative + navItem.href);
+      li.appendChild(anchor);
+      wrapper.appendChild(li);
+    }
 
-		const style = document.createElement('style');
-		style.textContent = `
+    const style = document.createElement("style");
+    style.textContent = `
 ul {
 width: 100%;
 list-style-type: none;
@@ -86,11 +86,10 @@ background-color: #111;
 .active {
 background-color: #04AA6D;
 }
-		`
-		shadow.appendChild(style);
-		shadow.appendChild(wrapper);
-	}
-
+		`;
+    shadow.appendChild(style);
+    shadow.appendChild(wrapper);
+  }
 }
 
-customElements.define('nav-bar', NavBar);
+customElements.define("nav-bar", NavBar);
