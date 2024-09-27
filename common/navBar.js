231e77fb4bd1b92a85207653a58e9f515cdce097
Joe Sharp <jsharp@caci.co.uk>
2024-09-27 09:14:21 +0100

    moving CSS into its own file

--- common/navBar.js
+++ common/navBar.js
@@ -1,3 +1,5 @@
+import sheet from "./navBar.css" with { type: "css" };
+
 export class NavBar extends HTMLElement {
   constructor(navItems) {
     super();
@@ -7,7 +9,6 @@ export class NavBar extends HTMLElement {
   connectedCallback() {
     // Create a shadow root
     const shadow = this.attachShadow({ mode: "open" });
-    const items = this.getAttribute("navItems");
     const current = this.getAttribute("current");
     const relative = this.getAttribute("relative") || ".";
 
@@ -25,43 +26,7 @@ export class NavBar extends HTMLElement {
       wrapper.appendChild(li);
     }
 
-    const style = document.createElement("style");
-    style.textContent = `
-ul {
-width: 100%;
-list-style-type: none;
-margin: 0;
-padding: 0;
-overflow: hidden;
-background-color: #333;
-}
-
-li.currentNav {
-	text-decoration: underline;
-text-decoration-color: white;
-}
-
-li {
-float: left;
-}
-
-li a {
-display: block;
-color: white;
-text-align: center;
-padding: 14px 16px;
-text-decoration: none;
-}
-
-li a:hover:not(.active) {
-background-color: #111;
-}
-
-.active {
-background-color: #04AA6D;
-}
-		`;
-    shadow.appendChild(style);
+    shadow.adoptedStyleSheets = [sheet];
     shadow.appendChild(wrapper);
   }
 }
